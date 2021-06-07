83748b6fef051033bc1e0a64b9e09b21415c578c
LENOVO <LENOVO@USUARIOO>
2021-06-07 12:39:47 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace App_1_3er_Bim
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine("Creado Por Pedro Javier Serrano Ramírez");
+            char[] dato = new char[10];
+
+
+            // dato[0] = 'a';
+
+            for (int i=0; i<=9; i++)
+            {
+                Console.WriteLine("i:" + i);
+                Console.Write("Inggrese una letra");
+                dato[i] = char.Parse(Console.ReadLine());
+            }
+
+        }
+    }
+}
